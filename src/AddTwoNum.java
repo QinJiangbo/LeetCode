cd9eedea58fb41e12f8df39b831c218a9b475a57
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-02 22:38:23 +0800

    add Solutions for problem AddTwoNumbers

--- src/AddTwoNum.java
+++ src/AddTwoNum.java
@@ -31,33 +31,6 @@ public class AddTwoNum {
         }
         return headNode.next;
     }
-
-
-    public static void main(String[] args) {
-        AddTwoNum addTwoNum = new AddTwoNum();
-        ListNode node1 = new ListNode(9);
-        ListNode node2 = new ListNode(9);
-        ListNode node3 = new ListNode(9);
-        ListNode node4 = new ListNode(9);
-        ListNode node5 = new ListNode(9);
-        ListNode node6 = new ListNode(9);
-        ListNode node7 = new ListNode(9);
-        ListNode node8 = new ListNode(9);
-        ListNode node9 = new ListNode(9);
-        ListNode node10 = new ListNode(9);
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = node6;
-        node6.next = node7;
-        node7.next = node8;
-        node8.next = node9;
-        node9.next = node10;
-
-        System.out.println(addTwoNum.addTwoNumbers(node1, node2).next.val);
-
-    }
 }
 
 /**
