9912defb0405c214e28b5ee42cb0b4c84dd1f28c
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-03 22:52:35 +0800

    add Solutions for Longest Substring problem

---
+++ src/code3/LongestSubStr.java
@@ -0,0 +1,21 @@
+package code3;
+
+/**
+ * @date: 03/03/2017 10:49 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+ *      Given a string, find the length of the longest substring without
+ *      repeating characters.
+        Examples:
+            Given "abcabcbb", the answer is "abc", which the length is 3.
+            Given "bbbbb", the answer is "b", with the length of 1.
+            Given "pwwkew", the answer is "wke", with the length of 3.
+            Note that the answer must be a substring,
+            "pwke" is a subsequence and not a substring.
+ */
+public class LongestSubStr {
+
+    public int lengthOfLongestSubstring(String s) {
+        return 1;
+    }
+}
