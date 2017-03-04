0634672ae6893e081b04545a39d0422795ecba7a
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-04 14:48:11 +0800

    add Solutions for Longest Palindromic String problem

---
+++ src/code5/LongestPalindromicStr.java
@@ -0,0 +1,24 @@
+package code5;
+
+/**
+ * @date: 04/03/2017 1:19 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+        Given a string s, find the longest palindromic substring in s.
+        You may assume that the maximum length of s is 1000.
+
+        Example:
+            Input: "babad"
+            Output: "bab"
+        Note: "aba" is also a valid answer.
+
+        Example:
+            Input: "cbbd"
+            Output: "bb"
+ */
+public class LongestPalindromicStr {
+
+    public String longestPalindrome(String s) {
+        return null;
+    }
+}
