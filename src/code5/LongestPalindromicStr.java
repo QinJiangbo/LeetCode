a7616a467f2e696e1b8a0d0f494b8c03e49e09d9
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-05 15:24:30 +0800

    add Solutions to Longest Palindromic String problem

--- src/code5/LongestPalindromicStr.java
+++ src/code5/LongestPalindromicStr.java
@@ -1,5 +1,6 @@
 package code5;
 
+
 /**
  * @date: 04/03/2017 1:19 PM
  * @author: qinjiangbo@github.io
@@ -18,7 +19,38 @@ package code5;
  */
 public class LongestPalindromicStr {
 
-    public String longestPalindrome(String s) {
-        return null;
+    /**
+     * Solutions:
+     *      1. Since it is palindromic string, so we can also find it in reverse string
+     *      2. reverse string s
+     *      3. find the longest common string of them
+     *      4. that's the longest palindromic string
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+        int len = s.length();
+        String rs = new StringBuffer(s).reverse().toString();
+        // find the longest common string
+        int start = -1, offset = 0;
+        for (int i = 0; i<len; i++) {
+            for (int j = 0; j<len; j++) {
+                // if the characters are equal
+                if (s.charAt(i) == rs.charAt(j)) {
+                    int k = 1;
+                    while ((i+k) < len && (j+k) < len) {
+                        if (s.charAt(i+k) == rs.charAt(j+k)) k++;
+                        else break;
+                    }
+                    if (offset < k) { start = i; offset = k; }
+                }
+            }
+        }
+        return s.substring(start, start + offset);
+    }
+
+    public static void main(String[] args) {
+        String s = "cbbd";
+        System.out.println(longestPalindrome(s));
     }
 }
