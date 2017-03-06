1169511adaa91b38f1459f2c7080f5e58055d288
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-06 22:25:40 +0800

    add Solution to Most Water Container Problem

--- src/MostWaterContainer/Solution.java
+++ src/MostWaterContainer/Solution.java
@@ -14,8 +14,54 @@ package MostWaterContainer;
  */
 public class Solution {
 
-    public int maxArea(int[] height) {
-
-        return 0;
+    /**
+       y ^
+         |
+         |     a2
+         |     |  a3          an
+         |  a1 |  |     a5    |
+         |  |  |  |  a4 |     |
+         |  |  |  |  |  | ..  |
+         --------------------------->
+         0   1  2  3  4  5 ..  n     x
+     get the total water for all the bars problem, It actually is to find
+     the any 2 bars ai, aj, which hold most water together with x-axis.
+     * @param height
+     * @return
+     */
+    public static int maxArea(int[] height) {
+        int len = height.length;
+        int maxArea = 0;
+        int i = 0, j = len - 1;
+        while (i < j) {
+            maxArea = Math.max(maxArea, Math.min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j]) { i++; } else { j--; }
+        }
+        return maxArea;
     }
+
+    /**
+     *  Time Limit Exceeded version
+     * @param height
+     * @return
+     */
+    @Deprecated
+    public int maxArea0(int[] height) {
+        int len = height.length;
+        int maxArea = 0;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                int minHeight = Math.min(height[i], height[j]);
+                int area = minHeight * (j - i);
+                maxArea = maxArea > area ? maxArea : area;
+            }
+        }
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 2, 3, 10, 5, 7, 8, 9};
+        System.out.println(maxArea(height));
+    }
+
 }
