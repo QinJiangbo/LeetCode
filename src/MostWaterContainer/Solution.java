79a6ac0647055e9dacb01f705b6e4778e69baf89
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-06 20:51:25 +0800

    add Solution to Most Water Container Problem

--- src/MostWaterContainer/Solution.java
+++ src/MostWaterContainer/Solution.java
@@ -3,6 +3,19 @@ package MostWaterContainer;
 /**
  * @date: 06/03/2017 5:29 PM
  * @author: qinjiangbo@github.io
+ * @description:
+        Given n non-negative integers a1, a2, ..., an,
+        where each represents a point at coordinate (i, ai).
+        n vertical lines are drawn such that the two endpoints of line i
+        is at (i, ai) and (i, 0). Find two lines, which together with x-axis
+        forms a container, such that the container contains the most water.
+
+        Note: You may not slant the container and n is at least 2.
  */
 public class Solution {
+
+    public int maxArea(int[] height) {
+
+        return 0;
+    }
 }
