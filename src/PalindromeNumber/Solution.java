86c64d5d32d59bfbfdb9db1b9571b4eb36fa209f
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-06 10:38:11 +0800

    add Solution to Palindrome Number Problem

--- src/PalindromeNumber/Solution.java
+++ src/PalindromeNumber/Solution.java
@@ -3,6 +3,23 @@ package PalindromeNumber;
 /**
  * @date: 06/03/2017 10:26 AM
  * @author: qinjiangbo@github.io
+ * @description:
+        Determine whether an integer is a palindrome. Do this without extra space.
  */
 public class Solution {
+
+    public static boolean isPalindrome(int x) {
+        int rx = 0, ax = x;
+        while (x != 0) {
+            int tail = x % 10;
+            rx = rx * 10 + tail;
+            x = x / 10;
+        }
+        System.out.println(rx);
+        return ax >= 0 && rx == ax;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(-2147447412));
+    }
 }
