ecd0bf78e83d37a8423bfe0c3d0872311ca346f3
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-09 11:22:00 +0800

    add Solution to Letter Combination of Phone Numbers Problem

--- src/LetterCombinations/Solution.java
+++ src/LetterCombinations/Solution.java
@@ -1,5 +1,7 @@
 package LetterCombinations;
 
+import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -22,7 +24,61 @@ import java.util.List;
  */
 public class Solution {
 
+    /**
+     * FIFO queue version
+     * @param digits
+     * @return
+     */
+    public List<String> letterCombinations0(String digits) {
+        LinkedList<String> result = new LinkedList<>();
+        String[] letters = new String[]{"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        result.add("");
+        int len = digits.length();
+        for (int i = 0; i < len; i++) {
+            int current = Character.digit(digits.charAt(i), 10);
+            while (result.peek().length() == i) { // keep consistent
+                String peek = result.remove();
+                for (char ch: letters[current].toCharArray()) {
+                    result.add(peek + ch);
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * backtracking version
+     * @param digits
+     * @return
+     */
     public List<String> letterCombinations(String digits) {
-        return null;
+        String[] letters = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        return backtracking(digits, 0, letters);
+    }
+
+    private List<String> backtracking(String digits, int dimension, String[] letters) {
+        List<String> lists = new LinkedList<>();
+        if (dimension >= digits.length()) { return new LinkedList<>(); }
+        int current = Character.digit(digits.charAt(dimension), 10);
+        char[] chars = letters[current].toCharArray();
+        for (char ch: chars) {
+            List<String> subLists = backtracking(digits, dimension + 1, letters);
+            if (subLists.size() == 0) {
+                StringBuilder stringBuilder = new StringBuilder();
+                lists.add(stringBuilder.append(ch).toString());
+            } else {
+                for (String str: subLists) {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    lists.add(stringBuilder.append(ch).append(str).toString());
+                }
+            }
+        }
+        return lists;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> lists = solution.letterCombinations0("");
+        System.out.println(Arrays.toString(lists.toArray()));
     }
 }
