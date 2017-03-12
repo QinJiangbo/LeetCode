19b23d8660133138d9bf0249481c68cec97b4f92
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-12 10:52:15 +0800

    add Solution to Valid Parentheses Problem

--- src/ValidParentheses/Solution.java
+++ src/ValidParentheses/Solution.java
@@ -3,6 +3,16 @@ package ValidParentheses;
 /**
  * @date: 11/03/2017 10:51 PM
  * @author: qinjiangbo@github.io
+ * @description:
+        Given a string containing just the characters '(', ')', '{', '}',
+        '[' and ']', determine if the input string is valid.
+
+        The brackets must close in the correct order, "()" and "()[]{}" are
+        all valid but "(]" and "([)]" are not.
  */
 public class Solution {
+
+    public boolean isValid(String s) {
+        return true;
+    }
 }
