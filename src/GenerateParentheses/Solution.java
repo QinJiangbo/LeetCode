56c3da0272b298a451e27fce5234c0a4621c5110
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-12 19:54:36 +0800

    add Solution to Merge Two Sorted Lists Problem

---
+++ src/GenerateParentheses/Solution.java
@@ -0,0 +1,27 @@
+package GenerateParentheses;
+
+import java.util.List;
+
+/**
+ * @date: 12/03/2017 7:39 PM
+ * @author: qinjiangbo@github.io
+   @description:
+        Given n pairs of parentheses, write a function to generate all combinations
+        of well-formed parentheses.
+
+        For example, given n = 3, a solution set is:
+
+           [
+               "((()))",
+               "(()())",
+               "(())()",
+               "()(())",
+               "()()()"
+           ]
+ */
+public class Solution {
+
+    public List<String> generateParenthesis(int n) {
+        return null;
+    }
+}
