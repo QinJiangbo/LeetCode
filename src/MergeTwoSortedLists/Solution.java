444b378494e2d3d9a6c4e5dcc8b2b675886d69a9
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-12 18:17:58 +0800

    add Solution to Merge Two Sorted Lists Problem

---
+++ src/MergeTwoSortedLists/Solution.java
@@ -0,0 +1,16 @@
+package MergeTwoSortedLists;
+
+/**
+ * @date: 12/03/2017 6:16 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+        Merge two sorted linked lists and return it as a new list.
+        The new list should be made by splicing together the nodes of
+        the first two lists.
+ */
+public class Solution {
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        return null;
+    }
+}
