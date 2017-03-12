b905a685087ea8483a7852e2e97fe81f586173ee
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-12 19:38:35 +0800

    add Solution to Merge Two Sorted Lists Problem

--- src/MergeTwoSortedLists/Solution.java
+++ src/MergeTwoSortedLists/Solution.java
@@ -10,7 +10,18 @@ package MergeTwoSortedLists;
  */
 public class Solution {
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        return null;
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) { return l2; }
+        if (l2 == null) { return l1; }
+
+        ListNode node;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            node = l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            node = l2;
+        }
+        return node;
     }
 }
