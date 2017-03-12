66d5d411b7cf964bd829e137e4299277d3335805
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-12 21:34:58 +0800

    add Solution to Generate Parenthesis Problem

--- src/GenerateParentheses/Solution.java
+++ src/GenerateParentheses/Solution.java
@@ -25,17 +25,43 @@ import java.util.List;
 public class Solution {
 
     /**
-     Let us consider an example to get clear view:
-     f(0): ""
-     f(1): "("f(0)")"
-     f(2): "("f(0)")"f(1), "("f(1)")"
-     f(3): "("f(0)")"f(2), "("f(1)")"f(1), "("f(2)")"
-     f(4): "("f(0)")"f(3), "("f(1)")"f(2), "("f(2)")"f(1), "("f(3)")"
-     So f(n) = "("f(0)")"f(n-1) , "("f(1)")"f(n-2) "("f(2)")"f(n-3) ... "("f(i)")"f(n-1-i) ... "(f(n-1)")"
+     * backtracking version
      * @param n
      * @return
      */
     public List<String> generateParenthesis(int n) {
+        List<String> list = new LinkedList<>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    private void backtrack(List<String> list, String str, int open, int close, int n) {
+        if (open == n && close == n) {
+            list.add(str);
+            return;
+        }
+
+        if (open < n) {
+            backtrack(list, str + "(", open + 1, close, n);
+        }
+        if (close < open) {
+            backtrack(list, str + ")", open, close + 1, n);
+        }
+    }
+
+    /**
+     * Dynamic Planning (DP) version
+     Let us consider an example to get clear view:
+     f(0): ""
+     f(1): "("f(0)")"f(0)
+     f(2): "("f(0)")"f(1), "("f(1)")"f(0)
+     f(3): "("f(0)")"f(2), "("f(1)")"f(1), "("f(2)")"f(0)
+     f(4): "("f(0)")"f(3), "("f(1)")"f(2), "("f(2)")"f(1), "("f(3)")"f(0)
+     So f(n) = "("f(0)")"f(n-1) "("f(1)")"f(n-2) "("f(2)")"f(n-3) ... "("f(i)")"f(n-1-i) ... "(f(n-1)")"f(0)
+     * @param n
+     * @return
+     */
+    public List<String> generateParenthesis0(int n) {
         List<List<String>> lists = new LinkedList<>();
         lists.add(Collections.singletonList(""));
 
