7d6c16a3f7800136224ba8011191f13964d97cca
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-14 14:32:38 +0800

    add Solution to Merge K Sorted Lists Problem

--- src/MergeKSortedLists/Solution.java
+++ src/MergeKSortedLists/Solution.java
@@ -12,6 +12,27 @@ import Commons.ListNode;
 public class Solution {
 
     public ListNode mergeKLists(ListNode[] lists) {
+        int len = lists.length;
+        int lo = 0, high = len -1, mid = (high - lo + 1) / 2;
+        return partition(lists, lo, mid, high);
+    }
+
+    private ListNode partition(ListNode[] lists, int lo, int mid, int high) {
         return null;
+    }
+
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) { return l2; }
+        if (l2 == null) { return l1; }
+
+        ListNode node;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            node = l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            node = l2;
+        }
+        return node;
     }
 }
