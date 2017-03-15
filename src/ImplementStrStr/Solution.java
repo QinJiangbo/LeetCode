cb4f234e63e878a4bd9f9326d5d1f3e5c4bfb7e6
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-16 01:21:42 +0800

    add Solution to Implement StrStr() Problem

---
+++ src/ImplementStrStr/Solution.java
@@ -0,0 +1,29 @@
+package ImplementStrStr;
+
+/**
+ * @date: 16/03/2017 12:18 AM
+ * @author: qinjiangbo@github.io
+ * @description:
+        Implement strStr().
+        Returns the index of the first occurrence of needle in haystack,
+        or -1 if needle is not part of haystack.
+ */
+public class Solution {
+
+    public static int strStr(String haystack, String needle) {
+        if (needle.equals("")) { return 0; }
+        if (haystack.equals("")) { return -1; }
+        int i, M = haystack.length();
+        int j, N = needle.length();
+        for (i = 0, j = 0; i < M && j < N; i++) {
+            if (haystack.charAt(i) == needle.charAt(j)) j++;
+            else { i -= j; j = 0; } // 显式回退
+        }
+        if (j == N) return i - N;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(strStr("mississippi", "issip"));
+    }
+}
