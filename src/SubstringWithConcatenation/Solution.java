c8601ccee40742ba06f02aa74f54ade608d09842
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-17 21:46:30 +0800

    add Solution to Substring with Concatenation of All Words Problem

--- src/SubstringWithConcatenation/Solution.java
+++ src/SubstringWithConcatenation/Solution.java
@@ -6,7 +6,7 @@ import java.util.List;
  * @date: 17/03/2017 9:30 PM
  * @author: qinjiangbo@github.io
  * @description:
-        You are given a string, s, and a list of words, words, that are all of
+        You are given a string, s, and a list of words, that are all of
         the same length. Find all starting indices of substring(s) in s that is
         a concatenation of each word in words exactly once and without any intervening
         characters.
