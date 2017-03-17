ef87ad774ae00c9e4aa1c49259f20e986fca492e
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-17 16:30:26 +0800

    add Solution to Divide Two Integers Problem

---
+++ src/DivideTwoIntegers/Solution.java
@@ -0,0 +1,15 @@
+package DivideTwoIntegers;
+
+/**
+ * @date: 17/03/2017 4:24 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+        Divide two integers without using multiplication, division and mod operator.
+        If it is overflow, return MAX_INT.
+ */
+public class Solution {
+
+    public int divide(int dividend, int divisor) {
+        return 0;
+    }
+}
