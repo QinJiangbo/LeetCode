e8c2a3331fc393d8f215e18ccb345d39a70b0a11
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-18 22:23:58 +0800

    add Solution to Substring with Concatenation of All Words Problem

--- src/SubstringWithConcatenation/Solution.java
+++ src/SubstringWithConcatenation/Solution.java
@@ -1,6 +1,6 @@
 package SubstringWithConcatenation;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * @date: 17/03/2017 9:30 PM
@@ -20,7 +20,55 @@ import java.util.List;
  */
 public class Solution {
 
-    public List<Integer> findSubstring(String s, String[] words) {
-        return null;
+    public static List<Integer> findSubstring(String s, String[] words) {
+        if (words.length == 0) { return null; }
+
+        List<Integer> indeces = new LinkedList<>();
+
+        int wlen = words[0].length();
+        int len = s.length();
+        Set<String> set = new HashSet<>();
+        // put words into set
+        for (String word: words) {
+            set.add(word);
+        }
+
+        Set<String> tmp = new HashSet<>();
+        boolean started = false;
+        for (int i = 0; i < (len - wlen + 1); i++) {
+            int j = i + wlen;
+            if (set.contains(s.substring(i, j))) {
+                if (tmp.contains(s.substring(i, j))) {
+                    started = false;
+                    i = i - tmp.size() * wlen;
+                    tmp.clear();
+                }
+                else {
+                    started = true;
+                    tmp.add(s.substring(i, j));
+                    i = j;
+                    if (tmp.size() == words.length){
+                        indeces.add(i - words.length * wlen);
+                        started = false;
+                        tmp.clear();
+                        i = i - (words.length - 1) * wlen;
+                    }
+                    i--;
+                }
+            }
+            else {
+                if (started) {
+                    i = i - tmp.size() * wlen;
+                    tmp.clear();
+                }
+            }
+        }
+        return indeces;
+    }
+
+    public static void main(String[] args) {
+        String s = "barfoofoobarthefoobarman";
+        String[] words = { "bar","foo","the" };
+        System.out.println(findSubstring(s, words).toString());
     }
 }
