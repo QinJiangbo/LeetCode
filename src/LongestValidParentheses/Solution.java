0000ba5d00569477d58c545dadf862db842016d0
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-21 21:18:09 +0800

    add Solution to Longest Valid Parentheses Problem

--- src/LongestValidParentheses/Solution.java
+++ src/LongestValidParentheses/Solution.java
@@ -4,7 +4,18 @@ package LongestValidParentheses;
  * @date: 21/03/2017 9:15 PM
  * @author: qinjiangbo@github.io
  * @description:
+        Given a string containing just the characters '(' and ')', find the length
+        of the longest valid (well-formed) parentheses substring.
 
+        For "(()", the longest valid parentheses substring is "()", which has
+        length = 2.
+
+        Another example is ")()())", where the longest valid parentheses substring
+        is "()()", which has length = 4.
  */
 public class Solution {
+
+    public int longestValidParentheses(String s) {
+        return 0;
+    }
 }
