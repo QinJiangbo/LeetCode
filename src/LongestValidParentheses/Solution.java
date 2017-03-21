ae162c43e1db2c1a322a7523b4c40c435def3aab
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-03-21 21:16:19 +0800

    add Solution to Next Permutation Problem

---
+++ src/LongestValidParentheses/Solution.java
@@ -0,0 +1,8 @@
+package LongestValidParentheses;
+
+/**
+ * @date: 21/03/2017 9:15 PM
+ * @author: qinjiangbo@github.io
+ */
+public class Solution {
+}
--- src/NextPermutation/Solution.java
+++ src/NextPermutation/Solution.java
@@ -1,5 +1,7 @@
 package NextPermutation;
 
+import java.util.Arrays;
+
 /**
  * @date: 19/03/2017 1:45 AM
  * @author: qinjiangbo@github.io
@@ -21,10 +23,57 @@ package NextPermutation;
 public class Solution {
 
     /**
-     * find next permutation for the nums
+     * find next permutation for the nums, the main idea of this problem is to
+     * find the least numbers which are over than the current one
+     * eg. 1,2,3 --> 1,3,2[least one larger than 123]
      * @param nums
      */
     public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length <= 1) { return; }
+        int i = nums.length - 1; // last index of the array
+        while (i > 0 && nums[i] <= nums[i - 1]) {
+            i--;
+        }
+        if (i > 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i - 1]) {
+                j--;
+            } // find the first one larger than nums[i-1]
+            swap(nums, j, i-1);
+        }
+        reverse(nums, i, nums.length - 1);
+    }
 
+    /**
+     * swap two numbers in the array
+     * @param nums
+     * @param i
+     * @param j
+     */
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    /**
+     * reverse the order of the array from index i...j
+     * @param nums
+     * @param i
+     * @param j
+     */
+    private void reverse(int[] nums, int i, int j) {
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {1, 1, 5};
+        solution.nextPermutation(nums);
+        System.out.println(Arrays.toString(nums));
     }
 }
