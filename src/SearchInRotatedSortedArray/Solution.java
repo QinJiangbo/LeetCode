4956270b16f8d3f2051209163444872b48fd4ef1
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-04-10 20:46:30 +0800

    add Solution to Search in Rotated Sorted Array Problem

--- src/SearchInRotatedSortedArray/Solution.java
+++ src/SearchInRotatedSortedArray/Solution.java
@@ -16,7 +16,27 @@ package SearchInRotatedSortedArray;
 public class Solution {
 
     public int search(int[] nums, int target) {
-
-        return 0;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) { return mid; }
+            // left side ordered
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] < target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            // right side ordered
+            else {
+                if (nums[mid] < target && target < nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 }
