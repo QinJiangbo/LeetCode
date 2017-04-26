ba4904a5e90fced7971f871b00587779433f785a
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-04-26 20:42:57 +0800

    add Solution to Pattern Matcher Problem

--- src/Interview/PatternMatcher.java
+++ src/Interview/PatternMatcher.java
@@ -7,6 +7,32 @@ package Interview;
 public class PatternMatcher {
 
     public static void main(String[] args) {
+        String text1 = "100-trade-done";
+        String text2 = "200-trade-done";
+        String text3 = "200-paid-done";
+        String pattern = "*trade-done";
+        System.out.println(filter(text1, pattern));
+        System.out.println(filter(text2, pattern));
+        System.out.println(filter(text3, pattern));
 
+    }
+
+    private static boolean filter(String text, String pattern) {
+        if (text.length() < pattern.length()) { return false; }
+        int j = 0; boolean flag = true;
+        for (int i = 0; i < pattern.length(); i++) {
+            if (pattern.charAt(i) == '?') {
+                j++;
+            } else if (pattern.charAt(i) == '*') {
+                while (text.charAt(j) != pattern.charAt(i+1)) {
+                    j++;
+                    if (j == text.length()) { flag = false; return flag; }
+                }
+            } else {
+                if (pattern.charAt(i) == text.charAt(j)) { j++; }
+                else {  flag = false; break; }
+            }
+        }
+        return flag;
     }
 }
