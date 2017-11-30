8cf1bd94c28cc373cd53a2cfd3c371cffa4c5b5f
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-11-30 16:41:06 +0800

    添加数独游戏的解答题目

---
+++ src/SudokuSolver/Solution.java
@@ -0,0 +1,20 @@
+package SudokuSolver;
+
+/**
+ * @date: 30/11/2017 4:39 PM
+ * @author: qinjiangbo@github.io
+ * @description:
+ *      Write a program to solve a Sudoku puzzle by filling the empty cells.
+        Empty cells are indicated by the character '.'.
+        You may assume that there will be only one unique solution.
+ */
+public class Solution {
+
+    /**
+     * 解数独
+     * @param board
+     */
+    public void solveSudoku(char[][] board) {
+
+    }
+}
