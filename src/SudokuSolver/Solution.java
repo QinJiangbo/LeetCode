59020de1cbc25e0785427b7774ec3ba606a32f2f
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-12-01 19:20:13 +0800

    添加数独游戏的解答题目

--- src/SudokuSolver/Solution.java
+++ src/SudokuSolver/Solution.java
@@ -15,6 +15,61 @@ public class Solution {
      * @param board
      */
     public void solveSudoku(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        solve(board);
+    }
 
+    /**
+     * 回溯法解数独问题
+     * @param board
+     * @return
+     */
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValidSudoku(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    // 1-9全部试过，都不行，说明这条线失败，直接返回false
+                    // 这里需要加一个判断减少回溯深度，否则会报Time Limit Exceed错误
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 判断数独是否合法，只需要判断这一个点添加以后是否合法
+     * @param board
+     * @param row
+     * @param col
+     * @param c
+     * @return
+     */
+    private boolean isValidSudoku(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++) {
+            if(board[i][col] != '.' && board[i][col] == c) {
+                return false; // 检查每一行
+            }
+            if(board[row][i] != '.' && board[row][i] == c) {
+                return false; // 检查每一列
+            }
+            if(board[3 * (row / 3) + i / 3][ 3 * (col / 3) + i % 3] != '.' &&
+                    board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) {
+                return false; // 检查 3*3 方块
+            }
+        }
+        return true;
     }
 }
