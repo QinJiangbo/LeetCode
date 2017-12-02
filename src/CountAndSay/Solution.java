8c954bed28ec6b9602baa7c914754f2de53e69c4
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-12-02 10:53:06 +0800

    添加Count And Say的题目

--- src/CountAndSay/Solution.java
+++ src/CountAndSay/Solution.java
@@ -29,4 +29,43 @@ package CountAndSay;
             Output: "1211"
  */
 public class Solution {
+
+    /**
+     * 根据前面给出的字符串找到规律，然后给出第N个数字对应的字符串
+     * @param n
+     * @return
+     */
+    public String countAndSay(int n) {
+        if (n <= 0) { return null; }
+        int i = 1;
+        String string = "";
+        while (i <= n) {
+            if (i == 1) {
+                string = "1";
+            } else {
+                string = convert(string);
+            }
+            i++;
+        }
+        return string;
+    }
+
+    /**
+     * 对前一个字符串进行CountAndSay
+     * @param str
+     * @return
+     */
+    public String convert(String str) {
+        StringBuffer stringBuffer = new StringBuffer();
+        int lastIndex = 0;
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) != str.charAt(i-1)) {
+                stringBuffer.append(i-lastIndex).append(str.charAt(i-1));
+                lastIndex = i;
+            }
+        }
+        // 加上最后断尾的
+        stringBuffer.append(str.length() - lastIndex).append(str.charAt(str.length() - 1));
+        return stringBuffer.toString();
+    }
 }
