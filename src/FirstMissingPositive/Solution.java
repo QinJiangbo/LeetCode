2923d7a01b92d4c43bdcc3c87c5b895a99f05c35
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-12-06 00:28:13 +0800

    添加FirstMissingPositive的题目解决方案

--- src/FirstMissingPositive/Solution.java
+++ src/FirstMissingPositive/Solution.java
@@ -20,6 +20,32 @@ public class Solution {
      * @return
      */
     public int firstMissingPositive(int[] nums) {
-        return 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 一直调整到当前值满足nums[nums[i]-1]==nums[i]为止
+            while (nums[i] > 0 && nums[i] <= nums.length
+                    && nums[nums[i] - 1] != nums[i]) {
+                swap(i, nums[i] - 1, nums);
+            }
+        }
+        int missing = nums.length;
+        // 哪一个地方的值缺了就选择这一块
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return missing + 1;
+    }
+
+    /**
+     * 交换数组里面两个数的值
+     * @param i
+     * @param j
+     * @param nums
+     */
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
